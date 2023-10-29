135944196609365eee687c899f2a0e38b9dfb118
Yushi Abe <handballer006@gmail.com>
2023-10-29 23:39:41 +0900

    :sparkles: add typescript gen code

--- gencode.sh
+++ gencode.sh
@@ -2,13 +2,11 @@
 
 PROTO_PATH=./proto/v1
 
+buf generate --path ${PROTO_PATH}
+
 # 
 # golang
 #
-rm -f gencode/go/proto/v1/*.pb.go
-
-buf generate --path ${PROTO_PATH}
-
 cd ./gencode/go/proto
 if [ ! -e ./go.mod ]; then
     echo "go.mod is not exist, init gomodule."
@@ -16,3 +14,8 @@ if [ ! -e ./go.mod ]; then
 fi
 
 go mod tidy
+
+#
+# TypeScript
+#
+tsc
