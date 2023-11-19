5f8e2413ea5ad70c277e2f52c3cc450989138212
Yushi Abe <handballer006@gmail.com>
2023-11-19 15:56:31 +0900

    :art: fix index.ts

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
-export * from './gencode/ts/proto/v1/notificator_connect'
-export * from './gencode/ts/proto/v1/notificator_pb'
+export * from './gencode/ts/proto/v1/notificator_connect';
+export * from './gencode/ts/proto/v1/notificator_pb';
